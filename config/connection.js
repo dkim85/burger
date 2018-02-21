f491711bf25d3f04f4eee9770f886d606b31193c
Danilo <kimhodori@gmail.com>
2018-02-20 23:51:18 -0800

    deploying on heroku- no success yet

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // this is going to house all my sequel connetion data
 // keep port # in mind next time
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host:"localhost",
-  port: 8889, 
-  user:"root",
-  password:"root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host:"localhost",
+//   port: 8889, 
+//   user:"root",
+//   password:"root",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 8889,
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err){
   if(err)throw err;
